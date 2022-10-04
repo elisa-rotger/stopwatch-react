bd26980603ba7f2ffc99ef39f8419a9188521589
Elisa Rotger <elisa.rotger@weareadaptive.com>
2022-10-04 09:31:25 +0200

    testing build

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // root: '/stopwatch-react/',
-  root: '/Users/elisa.rotger/Documents/Workspace/Projects/stopwatch-react/',
+  // root: '/Users/elisa.rotger/Documents/Workspace/Projects/stopwatch-react/',
+  base: '/stopwatch-react/',
   plugins: [react()],
 })
