f5a132f4e3c2b18705ea99e0d5c7309da9fccc72
Elisa Rotger <elisa.rotger@weareadaptive.com>
2022-10-04 11:05:13 +0200

    testing build

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,9 @@ git checkout -b main
 git add -A
 git commit -m 'deploy'
 
+git config user.name "Elisa Rotger"
+git config user.email "elisa.rotger@weareadaptive.com"
+
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
