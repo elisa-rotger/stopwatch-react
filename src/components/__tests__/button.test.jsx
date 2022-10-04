f4182a71b17b757dea635cc5bcc7b29d948542ea
Elisa <elisa.rotger@weareadaptive.com>
2022-10-04 12:42:55 +0200

    testing test

--- src/components/__tests__/button.test.jsx
+++ src/components/__tests__/button.test.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { render, screen, cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom'
-import Button from '../button'
+// import Button from '../button'
+import Button from 'src/components/button'
 
 // TODO: Write tests for the button
 
