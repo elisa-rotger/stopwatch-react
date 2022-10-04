0f828fcf1ac5df7b9a5774d49d49be3a56a033cc
Elisa <elisa.rotger@weareadaptive.com>
2022-10-04 13:57:59 +0200

    testging test

--- jest.config.mjs
+++ jest.config.mjs
@@ -88,7 +88,7 @@ export default {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/' },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
@@ -100,7 +100,7 @@ export default {
   // notifyMode: "failure-change",
 
   // A preset that is used as a base for Jest's configuration
-  // preset: 'babel-jest',
+  // preset: 'babel-preset-jest',
 
   // Run tests from one or more projects
   // projects: undefined,
@@ -157,7 +157,7 @@ export default {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  // testPathIgnorePatterns: ['<rootDir>/src', '<rootDir>/node_modules'],
+  testPathIgnorePatterns: ['<rootDir>/src', '<rootDir>/node_modules'],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
@@ -172,10 +172,7 @@ export default {
   transform: { '^.+\\.js$': 'babel-jest', '^.+\\.jsx$': 'babel-jest' },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  // transformIgnorePatterns: [
-  //   "/node_modules/",
-  //   "\\.pnp\\.[^\\/]+$"
-  // ],
+  // transformIgnorePatterns: ['<rootDir>/node_modules/'],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
