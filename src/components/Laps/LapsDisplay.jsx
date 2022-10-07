51f90a64c11466150138149743eed9697fd5ee28
Elisa <elisa.rotger@weareadaptive.com>
2022-10-07 12:49:05 +0200

    refactor: move lap logic to laps display component

--- src/components/Laps/LapsDisplay.jsx
+++ src/components/Laps/LapsDisplay.jsx
@@ -3,10 +3,73 @@ import { useEffect, useState } from 'react'
 import { getFormattedTime } from '../../utils/formatting-utils'
 import './LapsDisplay.css'
 
+const initialLapState = {
+  id: 1,
+  interval: 0,
+  allLaps: [],
+}
+
+const initialHighestLowestLapsState = {
+  highestLap: { id: undefined, interval: 0 },
+  lowestLap: { id: undefined, interval: Infinity },
+}
+
 function LapControls(props) {
-  const { lapInfo, highestLowestLaps } = props
+  const { elapsedTime, lapId } = props
+
+  const [lapTotalTime, setLapTotalTime] = useState(0)
+  const [lapInfo, setLapInfo] = useState(initialLapState)
+  const [highestLowestLaps, setHighestLowestLaps] = useState(
+    initialHighestLowestLapsState,
+  )
 
   const [scroll, setScroll] = useState(false)
+
+  useEffect(() => {
+    setLapInfo((previousLapInfo) => ({
+      ...previousLapInfo,
+      interval: elapsedTime - lapTotalTime,
+    }))
+  }, [elapsedTime, lapTotalTime])
+
+  useEffect(() => {
+    if (lapId === 1) {
+      resetLaps()
+    } else {
+      const newLap = { id: lapInfo.id, interval: lapInfo.interval }
+      setLapTotalTime(elapsedTime)
+      setLapInfo((previousLapInfo) => ({
+        id: lapId,
+        interval: elapsedTime - lapTotalTime,
+        allLaps: [
+          { id: previousLapInfo.id, interval: previousLapInfo.interval },
+          ...previousLapInfo.allLaps,
+        ],
+      }))
+      findHighestLowestLaps(newLap)
+    }
+  }, [lapId])
+
+  const resetLaps = () => {
+    setLapInfo(initialLapState)
+    setHighestLowestLaps(initialHighestLowestLapsState)
+    setLapTotalTime(0)
+  }
+
+  const findHighestLowestLaps = (newLap) => {
+    if (newLap.interval < highestLowestLaps.lowestLap.interval) {
+      setHighestLowestLaps((prev) => ({
+        ...prev,
+        lowestLap: newLap,
+      }))
+    }
+    if (newLap.interval > highestLowestLaps.highestLap.interval) {
+      setHighestLowestLaps((prev) => ({
+        ...prev,
+        highestLap: newLap,
+      }))
+    }
+  }
 
   useEffect(() => {
     document.querySelector('.lap-container').addEventListener('scroll', () => {
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,70 +6,12 @@ import TimerDisplay from '../Timer/TimerDisplay'
 import LapsDisplay from '../Laps/LapsDisplay'
 import Footer from '../Footer/Footer'
 
-const initialLapState = {
-  id: 1,
-  interval: 0,
-  allLaps: [],
-}
-
-const initialHighestLowestLapsState = {
-  highestLap: { id: undefined, interval: 0 },
-  lowestLap: { id: undefined, interval: Infinity },
-}
-
 function Main() {
-  // TODO: Move laps logic to lapsDisplay component
   const [elapsedTime, setElapsedTime] = useState(0)
-  const [lapTotalTime, setLapTotalTime] = useState(0)
-
-  const [lapInfo, setLapInfo] = useState(initialLapState)
-  const [highestLowestLaps, setHighestLowestLaps] = useState(
-    initialHighestLowestLapsState,
-  )
   const [lapId, setLapId] = useState(1)
-
-  useEffect(() => {
-    setLapInfo((previousLapInfo) => ({
-      ...previousLapInfo,
-      interval: elapsedTime - lapTotalTime,
-    }))
-  }, [elapsedTime, lapTotalTime])
-
-  useEffect(() => {
-    if (lapId !== 1) {
-      const newLap = { id: lapInfo.id, interval: lapInfo.interval }
-      setLapInfo((previousLapInfo) => ({
-        id: lapId,
-        interval: elapsedTime - lapTotalTime,
-        allLaps: [
-          { id: previousLapInfo.id, interval: previousLapInfo.interval },
-          ...previousLapInfo.allLaps,
-        ],
-      }))
-      setLapTotalTime(elapsedTime)
-      findHighestLowestLaps(newLap)
-    }
-  }, [lapId])
-
-  const findHighestLowestLaps = (newLap) => {
-    if (newLap.interval < highestLowestLaps.lowestLap.interval) {
-      setHighestLowestLaps((prev) => ({
-        ...prev,
-        lowestLap: newLap,
-      }))
-    }
-    if (newLap.interval > highestLowestLaps.highestLap.interval) {
-      setHighestLowestLaps((prev) => ({
-        ...prev,
-        highestLap: newLap,
-      }))
-    }
-  }
 
   const reset = () => {
     setElapsedTime(0)
-    setLapTotalTime(0)
-    setLapInfo(initialLapState)
     setLapId(1)
   }
 
@@ -81,7 +23,7 @@ function Main() {
         handleLap={() => setLapId((prevId) => prevId + 1)}
         handleReset={reset}
       />
-      <LapsDisplay lapInfo={lapInfo} highestLowestLaps={highestLowestLaps} />
+      <LapsDisplay elapsedTime={elapsedTime} lapId={lapId} />
       <Footer />
     </main>
   )
