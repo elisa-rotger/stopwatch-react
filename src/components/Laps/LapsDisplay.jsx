d870755b5b1a4c138fafe47c59b663bd9697ea9e
Elisa <elisa.rotger@weareadaptive.com>
2022-10-10 12:49:11 +0200

    refactor: highest/lowest useRef, added temporary comments

--- src/components/Laps/LapsDisplay.jsx
+++ src/components/Laps/LapsDisplay.jsx
@@ -3,17 +3,17 @@ import { useEffect, useState, useReducer, useRef } from 'react'
 import { getFormattedTime } from '../../utils/formatting-utils'
 import './LapsDisplay.css'
 
+/* Initial states */
 const initialHighestLowestLaps = {
   highestLap: { id: undefined, interval: 0 },
   lowestLap: { id: undefined, interval: Infinity },
 }
-
 const initialEmptyLapsState = [1, 2, 3, 4, 5, 6]
 
+/* useReducer functions */
 const init = (initialCount, lastID) => {
   return { interval: initialCount, id: lastID + 1 }
 }
-
 const reducerRunningLap = (state, action) => {
   switch (action.type) {
     case 'init':
@@ -32,24 +32,25 @@ const reducerRunningLap = (state, action) => {
 function LapControls(props) {
   const { elapsedTime, lapId } = props
 
+  const runningLapRef = useRef()
   const [stateRunningLap, dispatchRunningLap] = useReducer(reducerRunningLap, {}, init)
-  const ref = useRef(stateRunningLap)
 
   const [allLaps, setAllLaps] = useState([])
 
+  const highestLowestRef = useRef()
   const [highestLowestLaps, setHighestLowestLaps] = useState(initialHighestLowestLaps)
 
   const [emptyLaps, setEmptyLaps] = useState(initialEmptyLapsState)
   const [isScrolling, setIsScrolling] = useState(false)
 
   const findHighestLowestLaps = (newLap) => {
-    if (newLap.interval < highestLowestLaps.lowestLap.interval) {
+    if (newLap.interval < highestLowestRef.current.lowestLap.interval) {
       setHighestLowestLaps((prev) => ({
         ...prev,
         lowestLap: newLap,
       }))
     }
-    if (newLap.interval > highestLowestLaps.highestLap.interval) {
+    if (newLap.interval > highestLowestRef.current.highestLap.interval) {
       setHighestLowestLaps((prev) => ({
         ...prev,
         highestLap: newLap,
@@ -57,40 +58,50 @@ function LapControls(props) {
     }
   }
 
+  /* Track refs */
   useEffect(() => {
-    ref.current = stateRunningLap
+    highestLowestRef.current = highestLowestLaps
+  }, [highestLowestLaps])
+
+  useEffect(() => {
+    runningLapRef.current = stateRunningLap
   }, [stateRunningLap])
 
+  /* Keep running lap in time with elapsedTime */
   useEffect(() => {
     if (elapsedTime) dispatchRunningLap({ type: 'increment' })
   }, [elapsedTime])
 
+  /* Fires every time a lap is added or timer is reset */
   useEffect(() => {
-    /* Reset case */
-    if (lapId === 0) {
-      resetLaps()
-      setEmptyLaps(initialEmptyLapsState)
-    }
-    /* Adding laps (except first one -> handled in first useEffect in time with elapsed time) */
-    if (lapId > 1) {
-      /* Get last known value in runningLap state to create new static lap */
-      const newLap = ref.current
-      setAllLaps((prevAllLaps) => [newLap, ...prevAllLaps])
-      findHighestLowestLaps(newLap)
+    switch (lapId) {
+      /* Reset */
+      case 0:
+        resetLaps()
+        break
+      /* First running lap */
+      case 1:
+        setEmptyLaps((prevArray) => prevArray.slice(0, -1))
+        break
+      default:
+        const newLap = runningLapRef.current
+        setAllLaps((prevAllLaps) => [newLap, ...prevAllLaps])
+        findHighestLowestLaps(newLap)
 
-      dispatchRunningLap({ type: 'add lap' })
-      if (emptyLaps.length) setEmptyLaps((prevArray) => prevArray.slice(0, -1))
+        dispatchRunningLap({ type: 'add lap' })
+
+        setEmptyLaps((prevArray) => prevArray.slice(0, -1))
     }
   }, [lapId])
-
-  // TODO: Deps still to 'remove': 'emptyLaps.length' and 'findHighestLowestLaps'
 
   const resetLaps = () => {
     dispatchRunningLap({ type: 'reset' })
     setHighestLowestLaps(initialHighestLowestLaps)
     setAllLaps([])
+    setEmptyLaps(initialEmptyLapsState)
   }
 
+  /* Workaround to fade the scrollbar */
   useEffect(() => {
     document.querySelector('.lap-container').addEventListener('scroll', () => {
       setIsScrolling(true)
@@ -101,9 +112,12 @@ function LapControls(props) {
     })
   }, [])
 
+  /* Helper function to get highest / lowest class name */
   const getClassName = (id) => {
-    if (allLaps.length > 1 && highestLowestLaps.highestLap.id === id) return 'highest'
-    if (allLaps.length > 1 && highestLowestLaps.lowestLap.id === id) return 'lowest'
+    if (allLaps.length > 1 && highestLowestRef.current.highestLap.id === id)
+      return 'highest'
+    if (allLaps.length > 1 && highestLowestRef.current.lowestLap.id === id)
+      return 'lowest'
     return ''
   }
 
